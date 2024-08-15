8b38bf25e1a9f8de8102ced1d8ce336a00c64b10
SOWBURNIKA U N <124854143+SOWBURNIKA-NALLAMUTHU@users.noreply.github.com>
2024-08-15 23:35:56 +0530

    Delete petsback directory

--- petsback/petsback/src/main/java/com/example/petsback/repository/UserRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.petsback.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.petsback.model.User;
-
-public interface UserRepo extends JpaRepository<User, Long> {
-}
